523adc6d143a4f86664a96a4d12989b1ad953d8c
Tony DiCola <tony@tonydicola.com>
2014-03-19 15:04:56 -0700

    Update script to load FTDI drivers after exit.

--- run_macos.sh
+++ run_macos.sh
@@ -3,8 +3,16 @@ if [ "$(id -u)" != "0" ]; then
   echo "Must be run as root with sudo!"
   exit 1
 fi
+
 echo "Temporarily disabling FTDI drivers."
 kextunload -b com.apple.driver.AppleUSBFTDI
 kextunload /System/Library/Extensions/FTDIUSBSerialDriver.kext
 echo ""
+
 python run.py $@
+
+echo ""
+echo "Enabling FTDI drivers again."
+kextload -b com.apple.driver.AppleUSBFTDI
+kextload /System/Library/Extensions/FTDIUSBSerialDriver.kext
+
